99724fa796e6420d4b3832ec416cbaebad3abf31
Gabriel Nuñez <137582671+GabrielN2004@users.noreply.github.com>
2024-06-14 01:23:21 -0300

    Subo una actualizacion a la main con nuevos botones

--- template/javascript/index.js
+++ template/javascript/index.js
@@ -2,15 +2,18 @@
 var modal = document.getElementById("product-modal");
 
 // Obtén el botón que abre el modal
-var btn = document.getElementById("modal-1");
+var btn_1 = document.getElementById("modal-1");
+var btn_2 = document.getElementById("modal-2");
 
 // Obtén el elemento <span> que cierra el modal
 var span = document.getElementsByClassName("close-btn")[0];
 
 // Cuando el usuario haga clic en el botón, abre el modal
-btn.onclick = function() {
+btn_1.onclick = function() {
     modal.style.display = "block";
 }
+
+
 
 // Cuando el usuario haga clic en <span> (x), cierra el modal
 span.onclick = function() {
